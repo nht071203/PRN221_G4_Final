f39319d95dfe7e41bc23e3a72a3d33f8221e1a19
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2024-10-05 16:42:08 +0700

    Add singleton and example

---
+++ PRN221_G4_Final/PRN221_DataAccess/DAOs/SingletonBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN221_DataAccess.DAOs
+{
+    public class SingletonBase<T> where T : class, new()
+    {
+        private static T _instance;
+        private static readonly object _lock = new object();
+        public static Prn221Context _context { get; set; } = new Prn221Context();
+
+        public static T Instance
+        {
+            get {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new T();
+                    }
+                    return _instance;
+                }
+            }
+            set { _instance = value; } 
+        }
+    }
+}
