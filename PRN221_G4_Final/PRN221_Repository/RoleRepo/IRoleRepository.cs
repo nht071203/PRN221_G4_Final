a67e2c6d82758d20576c8db22ea73632efcc8cfa
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2024-10-05 17:00:52 +0700

    Example repository

---
+++ PRN221_G4_Final/PRN221_Repository/RoleRepo/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using PRN221_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN221_Repository.RoleRepo
+{
+    public interface IRoleRepository
+    {
+        Task<IEnumerable<Role>> GetListAll();
+    }
+}
---
+++ PRN221_G4_Final/PRN221_Repository/RoleRepo/RoleRepository.cs
@@ -0,0 +1,23 @@
+﻿using PRN221_DataAccess.DAOs;
+using PRN221_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN221_Repository.RoleRepo
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private RoleDAO roleDAO;
+        public RoleRepository(RoleDAO roleDAO) {
+            this.roleDAO = roleDAO;
+        }
+
+        public async Task<IEnumerable<Role>> GetListAll()
+        {
+            return await roleDAO.GetAll();
+        }
+    }
+}
