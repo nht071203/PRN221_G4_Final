bd0b1f000de58faf07414b61299fa55e38100e7a
uyenvuminh <vuminhuyen268@gmail.com>
2024-10-12 16:02:24 +0700

    them account+new+roleDAO

--- PRN221_G4_Final/PRN221_DataAccess/DAOs/NewsDAO.cs
+++ PRN221_G4_Final/PRN221_DataAccess/DAOs/NewsDAO.cs
@@ -14,5 +14,40 @@ namespace PRN221_DataAccess.DAOs
         {
             return await _context.News.ToListAsync();
         }
+        public async Task<IEnumerable<News>> GetAll()
+        {
+            return await _context.News.ToListAsync();
+        }
+
+        public async Task<News> FindById(int id)
+        {
+            var item = await _context.News.FirstOrDefaultAsync(obj => obj.NewsId == id);
+            if (item == null) return null;
+            return item;
+        }
+        public async Task Add(News item)
+        {
+            _context.News.Add(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(News item)
+        {
+            var existingItem = await FindById(item.NewsId);
+            if (existingItem == null) return;
+
+            _context.Entry(existingItem).CurrentValues.SetValues(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var item = await FindById(id);
+            if (item == null) return;
+
+            _context.News.Remove(item);
+            await _context.SaveChangesAsync();
+        }
     }
 }
+
