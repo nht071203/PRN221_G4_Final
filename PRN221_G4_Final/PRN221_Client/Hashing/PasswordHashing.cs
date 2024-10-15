9a19863dece1389bbbcd8132999f9aab7faa5ae7
thucnhce171748 <thucnhce171748@fpt.edu.vn>
2024-10-15 21:24:38 +0700

    login with google done

---
+++ PRN221_G4_Final/PRN221_Client/Hashing/PasswordHashing.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PRN221_Client.Hashing
+{
+    public class PasswordHashing
+    {
+        public static string HashPasswordMD5(string password)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+        public static string RandomPassword(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var password = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                password.Append(chars[random.Next(chars.Length)]);
+            }
+            return password.ToString();
+        }
+    }
+}
