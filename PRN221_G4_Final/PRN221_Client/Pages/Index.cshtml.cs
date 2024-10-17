6d980ffd6c729f5e438ce91c62e2ee1718e9ca83
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2024-10-17 23:24:50 +0700

    BackEnd: list post

--- PRN221_G4_Final/PRN221_Client/Pages/Index.cshtml.cs
+++ PRN221_G4_Final/PRN221_Client/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using PRN221_BusinessLogic.Interface;
 using PRN221_BusinessLogic.Service;
 using System.Security.Claims;
+using PRN221_Models.DTO;
 
 namespace PRN221_Client.Pages
 {
@@ -13,15 +14,18 @@ namespace PRN221_Client.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly IAccountService _accountService;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IPostService _postService;
         public string? Username { get; set; }
         public string? Role { get; set; }
         public string? FullName { get; set; }
+        public List<PostDTO> Posts { get; set; }
 
-        public IndexModel(IHttpContextAccessor httpContextAccessor, ILogger<IndexModel> logger, IAccountService accountService)
+        public IndexModel(IHttpContextAccessor httpContextAccessor, ILogger<IndexModel> logger, IAccountService accountService, IPostService postService)
         {
             _httpContextAccessor = httpContextAccessor;
             _logger = logger;
             _accountService = accountService;
+            _postService = postService;
         }
 
         public async Task<IActionResult> OnGet()
@@ -34,6 +38,9 @@ namespace PRN221_Client.Pages
             {
                 FullName = await _accountService.GetFullNameByUsername(Username);
             }
+
+            //LAY LIST POST KEM IMAGE
+            Posts = await _postService.GetListPostAndImage();
             
             return Page();
         }
---
+++ PRN221_G4_Final/PRN221_Repository/PostImageRepo/IPostImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using PRN221_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN221_Repository.PostImageRepo
+{
+    public interface IPostImageRepository
+    {
+        Task<IEnumerable<PostImage>> GetAll();
+        Task<IEnumerable<PostImage>> GetAllByPostId(int postId);
+    }
+}
