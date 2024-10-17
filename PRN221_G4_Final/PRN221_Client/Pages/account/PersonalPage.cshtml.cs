ff9d45d7a9ee86c2650172438bb183edbfc062ed
thucnhce171748 <thucnhce171748@fpt.edu.vn>
2024-10-17 20:12:49 +0700

    add UI personal page

---
+++ PRN221_G4_Final/PRN221_Client/Pages/account/PersonalPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Client.Pages.account
+{
+    public class PersonalPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
