50dbc4a042d7f718622bc92293b306f8082024c7
thucnhce171748 <thucnhce171748@fpt.edu.vn>
2024-10-17 22:49:35 +0700

    add UI profile

---
+++ PRN221_G4_Final/PRN221_Client/Pages/Profile/ListPhotos.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN221_BusinessLogic.Interface;
+using PRN221_Models.Models;
+
+namespace PRN221_Client.Pages.Profile
+{
+    public class ListPhotosModel : PageModel
+    {
+        private readonly IAccountService _accountService;
+
+        public ListPhotosModel(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        public Account Profile { get; set; }
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Profile = await _accountService.GetByIdAccount(id);
+            if (Profile == null)
+            {
+                return NotFound(); // Handle case when account is not found
+            }
+            return Page();
+        }
+    }
+}
