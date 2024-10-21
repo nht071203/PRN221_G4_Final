6ca1e716b69545e7d72fc7d0e626190d15d4023a
Tran Phuong Nam <namtpce170126@fpt.edu.vn>
2024-10-21 19:57:36 +0700

    Fix UI Register, and backend register

--- PRN221_G4_Final/PRN221_Client/Pages/Service/ListServices.cshtml.cs
+++ PRN221_G4_Final/PRN221_Client/Pages/Service/ListServices.cshtml.cs
@@ -142,7 +142,7 @@ namespace PRN221_Client.Pages.Service
             {
                 ServiceId = InputServiceId,
                 BookingBy = getAccId,
-                BookingAt = DateOnly.FromDateTime(DateTime.Now),
+                BookingAt = DateTime.Now,
                 BookingStatus = "sending",
                 IsDeletedFarmer = false,
                 Content = InputRequestContent,
